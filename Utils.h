854996ed2c07efd165875854e5d2d72185976ed8
seanchense <seanchense@outlook.com>
2014-12-23 18:53:52 +0800

    update

--- Utils.h
+++ Utils.h
@@ -8,16 +8,25 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+typedef void (^ZcBlock) (NSUInteger ret, id data);
 @interface Utils : NSObject
 
 //弹出提示
 + (void) warningUser:(UIViewController *) viewController msg:(NSString *) msg;
 + (void) warningUser:(UIViewController *) viewController msg:(NSString *) msg time:(NSInteger)time;
 + (void) warningUserAfterJump:(UIViewController *) viewController msg:(NSString *) msg time:(NSInteger)time;
+
 //在导航栏右边显示菊花
 + (UIActivityIndicatorView *) showIndicator:(UIViewController *) viewControllerindicator;
++ (UIActivityIndicatorView *) showIndicatorWithUINavigtationItem:(UINavigationItem *)navigationItem;
+
 //在导航栏右边隐藏菊花
 + (void) hideIndicator:(UIViewController *) viewControllerindicator indicator:(UIActivityIndicatorView *)indicator;
 + (void) hideIndicator:(UIViewController *) viewControllerindicator indicator:(UIActivityIndicatorView *)indicator insteadOf:(UIBarButtonItem *)btn;
+
+//在导航栏右边隐藏菊花 segue 为 modal 的形式
++ (void) hideIndicatorWithUINavigationItem:(UINavigationItem *)navigationItem indicator:(UIActivityIndicatorView *)indicator;
++ (void) hideIndicatorWithUINavigationItem:(UINavigationItem *)navigationItem indicator:(UIActivityIndicatorView *)indicator insteadOf:(UIBarButtonItem *)btn;
++ (void) getPosition:(UIViewController *) viewController block:(ZcBlock)block;
 @end
 
--- Utils.m
+++ Utils.m
@@ -8,7 +8,7 @@
 
 #import "Utils.h"
 #import "MBProgressHUD.h"
-
+#import "INTULocationManager.h"
 
 @implementation Utils
 
@@ -52,6 +52,17 @@
     return indicator;
 }
 
++ (UIActivityIndicatorView *) showIndicatorWithUINavigtationItem:(UINavigationItem *)navigationItem {
+    
+    UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(0, 0, 20, 20)];
+    indicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
+    UIBarButtonItem* btn_indicator = [[UIBarButtonItem alloc] initWithCustomView:indicator];
+    navigationItem.rightBarButtonItem = btn_indicator;
+    [indicator startAnimating];
+    
+    return indicator;
+}
+
 + (void) hideIndicator:(UIViewController *)viewController indicator:(UIActivityIndicatorView *)indicator{
     
     if (indicator) {
@@ -68,4 +79,55 @@
     }
 }
 
+
++ (void) hideIndicatorWithUINavigationItem:(UINavigationItem *)navigationItem indicator:(UIActivityIndicatorView *)indicator {
+    
+    if (indicator) {
+        [indicator stopAnimating];
+        
+    }
+}
+
++ (void) hideIndicatorWithUINavigationItem:(UINavigationItem *)navigationItem indicator:(UIActivityIndicatorView *)indicator insteadOf:(UIBarButtonItem *)btn {
+    
+    if (indicator) {
+        [indicator stopAnimating];
+        navigationItem.rightBarButtonItem = btn;
+    }
+}
+
+// 获取经纬度
++ (void)getPosition:(UIViewController *)viewController block:(ZcBlock)block{
+    INTULocationManager *locMgr = [INTULocationManager sharedInstance];
+    __block NSString* position;
+    [locMgr requestLocationWithDesiredAccuracy:INTULocationAccuracyHouse
+                                       timeout:2.0
+                          delayUntilAuthorized:YES  // This parameter is optional, defaults to NO if omitted
+                                         block:^(CLLocation *currentLocation, INTULocationAccuracy achievedAccuracy, INTULocationStatus status) {
+                                             if (status == INTULocationStatusSuccess) {
+                                                 // Request succeeded, meaning achievedAccuracy is at least the requested accuracy, and
+                                                 // currentLocation contains the device's current location.
+                                                 position = [NSString stringWithFormat:@"%f-%f",currentLocation.coordinate.longitude, currentLocation.coordinate.latitude];
+                                                 NSLog(@"获取经纬度成功 %@",position);
+                                                 block(0, position);
+                                                 
+                                             }
+                                             else if (status == INTULocationStatusTimedOut) {
+                                                 // Wasn't able to locate the user with the requested accuracy within the timeout interval.
+                                                 // However, currentLocation contains the best location available (if any) as of right now,
+                                                 // and achievedAccuracy has info on the accuracy/recency of the location in currentLocation.
+                                                 position = [NSString stringWithFormat:@"%f-%f",currentLocation.coordinate.longitude, currentLocation.coordinate.latitude];
+                                                 NSLog(@"获取经纬度超时，使用上一次的数据 %@",position);
+                                                 block(0, position);
+                                             }
+                                             else {
+                                                 // An error occurred, more info is available by looking at the specific status returned.
+                                                 [self warningUser:viewController msg:@"获取经纬度失败"];
+                                                 
+                                             }
+                                             
+                                         }];
+    
+    
+}
 @end
